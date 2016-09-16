31a3450d6fb487ac7b52bba74d6829fed754e047
Zachary Matthewstearn <zammo911@gmail.com>
2016-09-16 10:35:02 -0700

    begin prototyping front end: user may add words to the dictionary, and a list of those words is displayed on the home page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
@@ -9,8 +11,26 @@ public class App {
     staticFileLocation("/public");
 		String layout = "templates/layout.vtl";
 
-		get("/", (request,repsponse) -> {
+		get("/", (request,response) -> {
 			Map<String,Object> model = new HashMap<String,Object>();
+			model.put("template","templates/index.vtl");
+			return new ModelAndView(model,layout);
+		}, new VelocityTemplateEngine());
+
+		post("/", (request,response) -> {
+			Map<String,Object> model = new HashMap<String,Object>();
+
+			List<Word> words = new ArrayList<Word>();
+			if(request.session().attribute("words") == null){
+        List<Word> temp = new ArrayList<Word>();
+        request.session().attribute("words", temp);
+      }
+
+			words = request.session().attribute("words");
+			Word newWord = new Word(request.queryParams("inputWord"));
+			words.add(newWord);
+
+			model.put("words",words);
 			model.put("template","templates/index.vtl");
 			return new ModelAndView(model,layout);
 		}, new VelocityTemplateEngine());
